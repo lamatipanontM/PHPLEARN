0acef369fab9c5f50112de3616fa18395eb66b70
lamatipanontM <lamatipanont.m@oundleschool.org.uk>
2022-10-07 12:46:13 +0100

    test

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    <title>Page Title</title>
+
+    </head>
+    <body>
+        <form action="addusers.php">
+            First name:<input type="text" name= "forename"><br>
+            Last name:<input type= "text" name= "surename"><br>
+            Password:<input type= "password" name= "passwd"><br>
+            House:<input type="text" name="house"><br>
+            Year:<input type="text" name="year"><br>
+            <!--Creates a drop down list-->$_COOKIE
+            Gender:<select name="gneder">
+                <option value="M">Male</option>
+                <option value ="F">Female</option>
+    </body>
+</html>
