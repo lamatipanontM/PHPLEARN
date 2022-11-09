c1cdf0d3f291deb9d122957e71282cec45d9e202
lamatipanontM <lamatipanont.m@oundleschool.org.uk>
2022-11-09 10:34:24 +0000

    done!!!

--- index.php
+++ index.php
@@ -12,9 +12,17 @@
             Password:<input type= "password" name= "passwd"><br>
             House:<input type="text" name="house"><br>
             Year:<input type="text" name="year"><br>
-            <!--Creates a drop down list-->$_COOKIE
+            <!--Creates a drop down list-->
             Gender:<select name="gender">
                 <option value="M">Male</option>
                 <option value ="F">Femvale</option>
+            </select>
+        <br>
+        <!-- Next 3 lines creat a radio button which we can use to select the user role--->
+        <input type="radio" name="role" value="Pupil" checked> Pupil<br>
+        <input type="radio" name="role" value="Teacher" checked> Teacher<br>
+        <input type="radio" name="role" value="Admin" checked> Admin<br>
+        <input type="submit" value="Add User">
+        </form>
     </body>
 </html>
